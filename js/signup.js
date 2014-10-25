f39cfcd50d4d373a13231aa409aef38e1a52c13f
Robert Brooks <robertb1@uw.edu>
2014-10-25 12:50:11 -0700

    updated

--- js/signup.js
+++ js/signup.js
@@ -2,3 +2,49 @@
     Signup Form Script
     This script will load the state select list and validate the form before submission
 */
+
+"use strict"
+
+document.addEventListener('DOMContentLoaded', onReady);
+
+function onReady() {
+    var stateSelector = document.getElementById('stateSelector');
+    var i;
+    var option;
+    for (i = 0; i < usStates.length; i++) {
+        option = document.createElement('option');
+        option.innerHTML = usStates[i].name;
+        option.value = usStates[i].code;
+        stateSelector.appendChild(option);
+    }
+
+    var selectList = document.getElementById('occupation');
+    selectList.addEventListener('change', function() {
+        if (selectList.value === "other") {
+            document.getElementById('occupationOther').style.display = 'block';
+        }
+        else {
+            document.getElementById('occupationOther').style.display = 'none';
+            document.getElementById('occupationOther').value = '';
+        }
+    });
+
+    var exitButton = document.getElementById('exitButton');
+    exitButton.addEventListener('click', function() {
+        if (window.confirm("Are you sure you want to leave?")) {
+            window.location = 'http://google.com';
+        }
+    });
+
+    var signup = document.getElementById(signup);
+    signup.addEventListener('submit', onSubmit);
+}
+
+function onSubmit(eventObject) {
+    var fields = ['firstName', 'lastName', 'address1', 'city', 'state'];
+    var i;
+    var formValid = true;
+    for (i = 0; i <)
+
+}
+
